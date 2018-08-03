0dd9ab4441535442983fb7e93bfd24488772bb4e
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-03 13:50:11 +0530

    added basic MaterialApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,21 @@
-void main(){
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(GamePage());
+}
+
+class GamePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Minesweeper"),
+        ),
+        body: Container(
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
 }
