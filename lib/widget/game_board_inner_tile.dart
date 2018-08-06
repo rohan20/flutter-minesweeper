42315695436c6634c1f8bf49da109d404dacd5a4
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-06 15:40:42 +0530

    updated InnerTile widget to have support for color

--- lib/widget/game_board_inner_tile.dart
+++ lib/widget/game_board_inner_tile.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 
 class InnerTile extends StatelessWidget {
   final Widget child;
+  final Color color;
 
-  InnerTile({this.child});
+  InnerTile({this.child, this.color});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(1.0),
-      margin: const EdgeInsets.all(2.0),
-      height: 25.0,
-      width: 25.0,
+      margin: const EdgeInsets.all(1.0),
+      height: 30.0,
+      width: 30.0,
       child: child,
+      color: color == null ? Colors.grey[100] : color,
     );
   }
 }
