5078ae37c7ec0d499df686da18e59fe83dd19b0a
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-06 13:10:53 +0530

    updated GameBoardCoveredMineTile to be a StatelessWidget

---
+++ lib/widget/game_board_covered_mine_tile.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GameBoardCoveredMineTile extends StatelessWidget {
+  final bool flagged;
+  final int posX;
+  final int posY;
+
+  GameBoardCoveredMineTile({this.flagged, this.posX, this.posY});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
