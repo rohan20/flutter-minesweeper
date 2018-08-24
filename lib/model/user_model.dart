607db073b67b15fee23baa4c36bf83d49303c2d1
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-08-24 18:07:13 +0530

    setup user profile page

---
+++ lib/model/user_model.dart
@@ -0,0 +1,9 @@
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String imageUrl;
+  final int score;
+
+  User({this.id, this.name, this.email, this.imageUrl, this.score});
+}
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:minesweeper/model/user_model.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -13,7 +15,58 @@ class _ProfilePageState extends State<ProfilePage> {
         title: Text("Profile"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: Center(
+          child: StreamBuilder(
+            stream: Firestore.instance.collection("users").snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                DocumentSnapshot documentSnapshot = snapshot.data.documents[0];
+
+                User user = User(
+                  id: documentSnapshot["user_id"],
+                  name: documentSnapshot["name"],
+                  email: documentSnapshot["email"],
+                  imageUrl: documentSnapshot["image_url"],
+                  score: documentSnapshot["score"],
+                );
+
+                return _buildUserProfile(user);
+              } else if (snapshot.hasError)
+                return Text("false");
+              else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserProfile(User user) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 28.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 150.0,
+            height: 150.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(user.imageUrl),
+              ),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Text(user.name, style: TextStyle(fontSize: 24.0)),
+          SizedBox(height: 12.0),
+          Text("Highscore: ${user.score}", style: TextStyle(fontSize: 20.0)),
+          SizedBox(height: 12.0),
+          Text(user.email),
+        ],
+      ),
     );
   }
 }
